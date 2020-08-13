6611355085e6f3c106c1e6f87892013b8ae2de70
Mario Blunk <mario.blunk@blunk-electronic.de>
2020-08-13 14:46:47 +0200

    fix logo

--- install-desktop-icons.sh
+++ install-desktop-icons.sh
@@ -4,7 +4,7 @@ version=$1
 
 echo "installing EAGLE desktop icon" $version "..."
 
-icon=$HOME/Desktop/EAGLE.desktop
+icon=$HOME/Desktop/EAGLE_$version.desktop
 
 if [ -e $icon ]; then
 	{
@@ -16,10 +16,10 @@ fi
 echo [Desktop Entry] >> $icon
 echo Version=1.0 >> $icon
 echo Type=Application >> $icon
-echo Name=EAGLE >> $icon
+echo Name=EAGLE_$version >> $icon
 echo Comment= >> $icon
 echo Exec=eagle >> $icon
-echo Icon=/opt/eagle-$version/bin/splash.jpg >> $icon
+echo Icon=/opt/eagle-$version/bin/eagle-logo.png >> $icon
 echo Path= >> $icon
 echo Terminal=false >> $icon
 echo StartupNotify=false >> $icon
